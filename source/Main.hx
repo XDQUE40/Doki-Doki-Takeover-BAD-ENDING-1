0431ee665fe4534e30197b0d6cd7ba2a4d649e52
XDQUE40 <116643129+XDQUE40@users.noreply.github.com>
2022-11-09 21:47:31 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -74,14 +74,14 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+		
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		if (fpsVar != null)
 		{
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
-		#end
+		
 
 		#if html5
 		FlxG.mouse.visible = false;
